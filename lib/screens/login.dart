c9b8875bfd80b5ee292efa0523be3481f3701bcd
Mahmoud <mahmoudsamymahmoud01@gmail.com>
2023-10-08 21:58:14 +0300

    feat: add login screen and app bar

---
+++ lib/screens/login.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  const Login({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: TextButton.icon(
+          icon: const Icon(
+            Icons.arrow_back_ios,
+            color: Colors.black,
+          ),
+          onPressed: () {},
+          label: const Text(
+            'Login',
+            style: TextStyle(
+                color: Colors.black,
+                fontSize: 21.5,
+                fontWeight: FontWeight.normal),
+          ),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0,
+      ),
+    );
+  }
+}
