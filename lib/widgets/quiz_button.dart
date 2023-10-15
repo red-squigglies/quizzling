f124807d315f62ea28360c3fca39c74b7c7a19bf
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2023-10-15 21:17:55 +0300

    chore: add loading & disabled to quiz button

--- lib/widgets/quiz_button.dart
+++ lib/widgets/quiz_button.dart
@@ -5,23 +5,37 @@ class QuizButton extends StatelessWidget {
     super.key,
     required this.onPressed,
     required this.text,
+    this.disabled = false,
+    this.loading = false,
   });
 
   final void Function() onPressed;
   final String text;
+  final bool disabled;
+  final bool loading;
+
+  void handlePress() {
+    if (!loading) onPressed();
+  }
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 60,
       child: ElevatedButton(
-        onPressed: onPressed,
+        onPressed: disabled ? null : handlePress,
         style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
-        child: Text(
-          text,
-          style: const TextStyle(fontSize: 16),
-          textAlign: TextAlign.center,
-        ),
+        child: loading
+            ? const SizedBox(
+                height: 30,
+                width: 30,
+                child: CircularProgressIndicator(color: Colors.white),
+              )
+            : Text(
+                text,
+                style: const TextStyle(fontSize: 16),
+                textAlign: TextAlign.center,
+              ),
       ),
     );
   }
