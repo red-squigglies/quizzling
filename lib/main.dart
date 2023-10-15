ce3f6a43c22727d691ff78bdae5e95ec8070095c
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2023-10-15 19:36:25 +0300

    chore: configure firebase app
    Co-authored-by: Mahmoud Samy <mahmoudsamyhamed@users.noreply.github.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'package:quizzling/config/firebase_options.dart';
 import 'package:quizzling/screens/login_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const QuizzlingApp());
 }
 
